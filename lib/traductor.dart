0b4054b25da5049b6d94b66bb4baf0cd86361dda
Edith008 <edith.sossa.c98@gmail.com>
2024-03-12 19:19:23 -0400

    arreglos

--- lib/traductor.dart
+++ lib/traductor.dart
@@ -15,7 +15,7 @@ class _TraductorState extends State<Traductor> {
 
   final String getUrl = 'https://text-translator2.p.rapidapi.com/getLanguages';
   final String postUrl = 'https://text-translator2.p.rapidapi.com/translate';
-  final String apiKey = 'asdfghjk';
+  final String apiKey = '1bc8398281msh2231918aaa711b5p193962jsnd5d145dc100f';
 
   @override
   void initState() {
@@ -61,60 +61,77 @@ class _TraductorState extends State<Traductor> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text('  TRADUCTOR  '),
+        title: Text('TRADUCTOR', style: TextStyle(fontWeight: FontWeight.bold)),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 20),
-          Text('Traducir del:', style: TextStyle(fontSize: 16)),
-          DropdownButton<String>(
-            value: idiomaInicio,
-            onChanged: (newValue) {
-              setState(() {
-                idiomaInicio = newValue!;
-              });
-            },
-            items: languages.map<DropdownMenuItem<String>>((Map<String, dynamic> language) {
-              return DropdownMenuItem<String>(
-                value: language['code'],
-                child: Text(language['name']),
-              );
-            }).toList(),
-          ),
-          SizedBox(height: 20),
-          Text('al:', style: TextStyle(fontSize: 16)),
-          DropdownButton<String>(
-            value: idiomaFinal,
-            onChanged: (newValue) {
-              setState(() {
-                idiomaFinal = newValue!;
-              });
-            },
-            items: languages.map<DropdownMenuItem<String>>((Map<String, dynamic> language) {
-              return DropdownMenuItem<String>(
-                value: language['code'],
-                child: Text(language['name']),
-              );
-            }).toList(),
-          ),
-          TextField(
-            controller: textEditingController,
-            decoration: InputDecoration(labelText: 'Ingresa el texto'),
+      
+      body: Padding(
+        padding: const EdgeInsets.all(20.0), 
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Text('Traducir del:', style: TextStyle(fontSize: 16)),
+                SizedBox(width: 30),
+                DropdownButton<String>(
+                  value: idiomaInicio,
+                  onChanged: (newValue) {
+                    setState(() {
+                      idiomaInicio = newValue!;
+                    });
+                  },
+                  items: languages.map<DropdownMenuItem<String>>((Map<String, dynamic> language) {
+                    return DropdownMenuItem<String>(
+                      value: language['code'],
+                      child: Text(language['name']),
+                    );
+                  }).toList(), 
+                ),
+              ],
+            ),
+
+            Row(
+              children: [
+                Text('al:', style: TextStyle(fontSize: 16)),
+                SizedBox(width: 30),
+                DropdownButton<String>(
+                  value: idiomaFinal,
+                  onChanged: (newValue) {
+                    setState(() {
+                      idiomaFinal = newValue!;
+                    });
+                  },
+                  items: languages.map<DropdownMenuItem<String>>((Map<String, dynamic> language) {
+                    return DropdownMenuItem<String>(
+                      value: language['code'],
+                      child: Text(language['name']),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 30),
+            TextField(
+              controller: textEditingController,  
+              decoration: InputDecoration(labelText: 'Ingresa el texto'), 
+            ),
             
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-               translateText(textEditingController.text);
-            },
-            child: Text('Traducir'),
-          ),
-          SizedBox(height: 20),
-          Text('TRADUCCION:', style: TextStyle(fontSize: 18)),
-          SizedBox(height: 10),
-          Text(textoTraducido),
-        ],
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                translateText(textEditingController.text);
+              },
+              child: Text('Traducir'),
+            ),
+            SizedBox(height: 20),
+            Text('TRADUCCION:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,)),
+            SizedBox(height: 10),
+            Text(textoTraducido),
+          ],
+        ),
       ),
     );
   }
